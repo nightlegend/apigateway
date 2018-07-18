8401f3a177e1d0d446ca8f0b46888aa597b282cd
guoda2 <david.guo@cargosmart.com>
2018-07-18 15:02:57 +0800

    remove unused  file

--- core/module/UserInfo.go
+++
@@ -1,8 +0,0 @@
-package module
-
-// UserInfo is define user info struct.
-type UserInfo struct {
-	USERNAME string `json:"userName" binding:"required"`
-	PASSWORD string `json:"password" binding:"required"`
-	EMAIL    string `json:"email" binding:"required"`
-}
