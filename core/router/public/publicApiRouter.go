fa2a71fa5c8a935d16bb74b679377ba36d8d038d
David Guo <david.guo@cargosmart.com>
2018-08-24 14:46:04 +0800

    refine public api router code

--- core/router/public/publicApiRouter.go
+++ core/router/public/publicApiRouter.go
@@ -5,8 +5,6 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
 	"github.com/nightlegend/apigateway/core/api/docker"
 	"github.com/nightlegend/apigateway/core/api/users"
@@ -14,16 +12,22 @@ import (
 )
 
 var (
-	// User services
 	uis  users.UserInfoService
 	flag int
 )
 
-// APIRouter is route public router
+// APIRouter define router from here, you can add new api about your new services.
 func APIRouter(router *gin.Engine) {
 	log.Info("start init public router.......")
-	store, _ := redis.NewStore(10, "tcp", "localhost:6379", "", []byte("secret"))
-	router.Use(sessions.Sessions("mysession", store))
+	/*
+		// TO-DO: cache store solution
+		store, err := redis.NewStore(10, "tcp", "localhost:6379", "", []byte("secret"))
+		if err != nil {
+			return
+		}
+		router.Use(sessions.Sessions("mysession", store))
+
+	*/
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "welcome to apigateway, you can find you want here!!!", "userInfo": "Hello World!!!"})
@@ -31,18 +35,20 @@ func APIRouter(router *gin.Engine) {
 
 	router.POST("/login", func(c *gin.Context) {
 		c.BindJSON(&uis)
-		session := sessions.Default(c)
-		v := session.Get(uis.USERNAME)
-		if v == nil {
-			flag = uis.Login()
-			session.Set(uis.USERNAME, uis.USERNAME)
-			session.Save()
-			log.Println("Try login and save session in session store.")
-		} else {
-			flag = consts.SUCCESS
-			log.Println("Have a session in session store.")
-		}
-
+		/*
+			// TO-DO: cache user login session.
+			session := sessions.Default(c)
+			if session.Get(uis.USERNAME) == nil {
+				flag = uis.Login()
+				session.Set(uis.USERNAME, uis.USERNAME)
+				session.Save()
+				log.Println("Try login and save session in session store.")
+			} else {
+				flag = consts.SUCCESS
+				log.Println("Have a session in session store.")
+			}
+		*/
+		flag = uis.Login()
 		switch flag {
 		case consts.SUCCESS:
 			c.JSON(http.StatusOK, gin.H{"code": consts.SUCCESS, "Message": "Login Successful", "tooken": ""})
@@ -57,8 +63,7 @@ func APIRouter(router *gin.Engine) {
 
 	router.POST("/register", func(c *gin.Context) {
 		c.BindJSON(&uis)
-		result := uis.Register()
-		if result {
+		if uis.Register() {
 			c.JSON(http.StatusOK, gin.H{"statusCode": http.StatusOK, "message": "Welcome " + uis.USERNAME + ",you have login successful!"})
 		} else {
 			c.JSON(http.StatusExpectationFailed, gin.H{"errorMessage": "Rigster failed "})
@@ -67,8 +72,7 @@ func APIRouter(router *gin.Engine) {
 
 	router.POST("/update", func(c *gin.Context) {
 		c.BindJSON(&uis)
-		res := uis.UpdateUserInfo()
-		if res {
+		if uis.UpdateUserInfo() {
 			c.JSON(http.StatusOK, gin.H{"statusCode": http.StatusOK, "message": uis.USERNAME + ",you have update information successfully!"})
 		} else {
 			c.JSON(http.StatusExpectationFailed, gin.H{"errorMessage": "update information failed, please contract admin help..."})
@@ -76,8 +80,7 @@ func APIRouter(router *gin.Engine) {
 	})
 
 	router.GET("/queryAll", func(c *gin.Context) {
-		users := uis.QueryAllAccountInfo()
-		c.JSON(http.StatusOK, gin.H{"statusCode": http.StatusOK, "message": "Query done, check console", "userList": users})
+		c.JSON(http.StatusOK, gin.H{"statusCode": http.StatusOK, "message": "Query done, check console", "userList": uis.QueryAllAccountInfo()})
 	})
 
 	router.GET("/getImageTagInfo", func(c *gin.Context) {
