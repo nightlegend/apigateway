15c9a724be14439a11e0b6bb790a8cf4f0f21b4a
David guo <david.guo18@yahoo.com>
2019-03-25 15:35:26 +0800

    update running testing configure

--- coverage.sh
+++ coverage.sh
@@ -5,7 +5,7 @@ set -e
 echo "mode: count" > coverage.out
 
 for d in $(go list ./...|grep -E 'utils$'); do
-    go test -v -covermode=count -coverprofile=profile.out $d
+    go test -v -mod=vendor -covermode=count -coverprofile=profile.out $d
     if [ -f profile.out ]; then
         cat profile.out | grep -v "mode:" >> coverage.out
         rm profile.out
